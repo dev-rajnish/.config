6c1a4193d900e207d53973c9fb28e84c6e83148a
Rajnish  Kumar <devrajnish@proton.me>
2023-11-06 19:23:35 +0530

    alias_fish

--- fish/functions/alias_fish.fish
+++ fish/functions/alias_fish.fish
@@ -2,19 +2,33 @@ function alias_fish
 alias s='sudo'
 alias zypper='sudo zypper'
 alias z='sudo zypper'
+
 alias ls='eza --icons -1 -a'
 alias la='eza -a --icons -1'
 alias ll='eza -lah --icons'
 alias rm='rm -i'
+alias batlst='bat --theme "Monokai Extended" --paging never --style=grid,header'
+
+
 alias vi='lvim'
 alias vim='lvim'
 aliad nvim='lvim'
-alias c='/home/rajnish/data/bash-script/clang-16.sh'
+alias cx='/home/rajnish/data/codes/c/auto-run-sh/clang-17.sh'
+alias lc='/home/rajnish/data/bash-script/l.sh'
 alias nano='lvim'
+
 alias add-alias='lvim ~/.config/fish/functions/alias_fish.fish'
 alias bashrc='lvim ~/.config/fish/config.fish'
-alias code-c='cd ~/data/codes/c/'
-alias cx='cd ~/data/codes/c/cx'
+
+alias c-code='cd ~/data/codes/c/'
+alias code-c='code ~/data/codes/c'
+alias c-r='cd ~/data/codes/c/rough'
 alias d='cd ~/data'
+
 alias cl='clear; ls -a' 
+alias clearx='clear -x'
+
+alias dwagent='cd ~/Public/ && sudo ./dwagent.sh'
+
+
 end 
