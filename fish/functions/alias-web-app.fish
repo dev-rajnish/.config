3652a3c8a055f039bd19fd742c26f6d6c813581d
rajnish kumar <dev.rajnish@proton.me>
2024-09-01 11:22:15 +0530

    organised alias

--- fish/functions/alias-web-app.fish
+++ fish/functions/alias-web-app.fish
@@ -3,4 +3,4 @@ alias yt='firefox --new-window https://youtube.com'
 #plain
 alias plain='firefox --new-window https://192.168.154.66:8443/'
 #chat GPT
-alias gpt='https://chatgpt.com/'
+alias gpt='firefox --new-window https://chatgpt.com/'
