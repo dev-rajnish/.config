e4aae4f57102059de9f3dfb290aeb0e8ba3a2d5f
dev-rajnish <dev.rajnish@proton.me>
2024-09-25 16:57:22 +0530

    udated alias and fish/function

--- fish/functions/alias_fish.fish
+++ fish/functions/alias_fish.fish
@@ -6,25 +6,25 @@ alias in="$HOME/my/bash-exec/zypper-log/install-with-logging"
 alias un="$HOME/my/bash-exec/zypper-log/uninstall-with-logging"
 ## -- git
 alias github="f2g"
-alias githuba="f2g ."
+alias github_all="f2g ."
 alias gits="git status"
 alias gita="git add ."
 
 alias my="cd $HOME/my"
-alias cpwd="echo -----------------------------;echo At : $PWD;echo ----------------------------"
 alias batc="bat --theme='Monokai Extended' --paging never --style=grid,header"
 alias ok="clear"
-alias ls="eza --icons -1 -a ; cpwd"
-alias l="eza --icons -1 -a  ; cpwd"
-alias ll="eza -lah --icons ; cpwd"
+alias ls="eza --icons -1 -a"
+alias l="eza --icons -1 -a"
+alias ll="eza -lah --icons"
 alias rm="rm -i"
 
-alias add_alias="vim ~/.config/fish/functions/alias_fish.fish"
-alias fishrc="vim ~/.config/fish/config.fish"
+alias add_alias="vi ~/.config/fish/functions/alias_fish.fish"
+alias fishrc="vi ~/.config/fish/config.fish"
 
 alias cl="clear; ls -a"
 alias clearx="clear -x"
 
 alias fish_greet="sudo vim /usr/share/fish/functions/fish_greeting.fish"
+alias pkg_list="glow ~/my/bash-exec/zypper-log/pkg-list.txt"
 #---------------------
 end
--- fish/functions/executables.fish
+++ fish/functions/executables.fish
@@ -2,5 +2,6 @@ function executables
 #------------
 #set PATH $PATH ~/.local/bin
 set PATH $PATH ~/.bun/bin
+set PATH $PATH ~/my/pkg
 #-------------
 end
