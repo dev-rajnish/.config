9590daf1c9b84eedde5c7392ffbeb5de6fd34246
dev-rajnish <dev.rajnish@proton.me>
2024-11-01 01:57:19 +0530

    Auto Commit and Push

--- fish/alias_list.fish
+++ fish/alias_list.fish
@@ -4,8 +4,6 @@ alias fish_dir 'cd ~/.config/fish/'
 
 alias tm 'tmux new -s pre 2>/dev/null ; tmux attach -t pre'
 
-alias ftg-after-vim '~/my/bash-exec/dir-to-gh/d2g-after-vim'
+alias n 'neo . && gh-this-dir'
 
-alias n 'neo . && ftg-after-vim'
-
-alias r 'ranger'
+alias r ranger
