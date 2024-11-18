8ddbd6a5ca92ede94ee220804ddeea178efaeb94
dev-rajnish <dev.rajnish@proton.me>
2024-11-19 02:19:46 +0530

    Auto Commit and Push

--- ba-sh/needed-pkg.sh
+++ ba-sh/needed-pkg.sh
@@ -28,22 +28,26 @@ wlr-randr \
 gcc \
 wl-clipboard \
 
-
-echo "3. Installing getnf -- for installing nerd-fonts"
+#=====
+echo "| 3. | Installing getnf -- for installing nerd-fonts"
 curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
 getnf
 
-echo "4. Moving to .config/"
+#=====
+echo "| 4. | Moving to .config/"
 cd ~/.config/
 git init
 git remote add origin "https://github.com/dev-rajnish/.config.git"
 
+#======
 ## -- cleaning .config for fetch/pull
-echo "5. mkdir -p ~/.config/before-needed-pkg.sh"
+echo "| 5. | mkdir -p ~/.config/before-needed-pkg"
 mkdir -p  before-needed-pkg
 
-echo "6. cleaning .config for fetch/pull"
-cp -rv mako/ walls/ -t before-needed-pkg
+#======
+echo "| 6. | cleaning .config for fetch/pull"
+mv  mako/ walls/ -t before-needed-pkg && echo cleaned || \
+  echo  "==X=X== | Error cleaning"
 
 
 
