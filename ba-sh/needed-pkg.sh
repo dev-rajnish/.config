dd6bcd7cd15ea33951f6f7dd3e12874590d0f34a
dev-rajnish <dev.rajnish@proton.me>
2024-11-19 00:57:53 +0530

    Auto Commit and Push

---
+++ ba-sh/needed-pkg.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo zypper ref
+sudo zypper in --no-recommends --details -y \
+ curl \
+fish \
+neovim \
+git \
+git-core \
+gh \
+hyprland \
+hyprland-devel \
+kitty \
+mako \
+sway \
+waybar \
+wofi \
+brightnessctl \
+xplr \
+glow \
+bat \
+eza \
+zoxide \
+libnotify- tools \
+wlr-randr \
+gcc \
+wl-clipboard \
+
+
+
