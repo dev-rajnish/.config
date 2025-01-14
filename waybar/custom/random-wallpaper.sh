3450da15f144af5fc5cf422a97aedef2b5063b8d
dev-rajnish <dev.rajnish@proton.me>
2025-01-14 19:05:23 +0530

    Auto Commit and Push

--- waybar/custom/random-wallpaper.sh
+++ waybar/custom/random-wallpaper.sh
@@ -2,4 +2,4 @@
 
 WALLPAPER="$(find $HOME/.config/walls/ -type f | shuf -n 1)"
 
-swww img "$WALLPAPER" &
+swww img "$WALLPAPER" -t any --transition-step 200 --transition-duration 1  --transition-fps 60 &
