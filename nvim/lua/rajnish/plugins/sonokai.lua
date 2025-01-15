00917d92ca78268241075179d40c2a11c6bc745d
dev-rajnish <dev.rajnish@proton.me>
2025-01-16 05:13:59 +0530

    ok

--- nvim/lua/rajnish/plugins/sonokai.lua
+++ nvim/lua/rajnish/plugins/sonokai.lua
@@ -6,7 +6,7 @@ return {
 		-- Optionally configure and load the colorscheme
 		-- directly inside the plugin declaration.
 		vim.g.sonokai_style = "maia" -- andromeda, atlantis, maia
-		vim.g.sonokai_transparent_background = 0
+		vim.g.sonokai_transparent_background = 2
 		vim.g.sonokai_disable_terminal_colors = 0
 		vim.g.sonokai_enable_italic = true
 		vim.cmd.colorscheme("sonokai")
